751f3359db4ac8c12979058029882618b7a89a2a
Gamedenied <jbanta@student.sussex.edu>
2020-01-22 21:23:59 -0500

    this one

--- Coms223Lab1/src/edu/sussex/coms223/lab1/ArrayList.java
+++ Coms223Lab1/src/edu/sussex/coms223/lab1/ArrayList.java
@@ -1,5 +1,56 @@
 package edu.sussex.coms223.lab1;
 
-public class ArrayList {
+import java.util.Arrays;
+
+public class ArrayList <E> implements List <E> {
+	
+	private Object[] Data = new Object[0]; 
+	
+	
+	@Override
+	public boolean add(E e) {
+		if(e== null) 
+		 throw new IllegalArgumentException ("null vaule not allowed");
+		 Data= Arrays.copyOf(Data, Data.length);
+		 Data[Data.length-1]=e;
+		 return true;
+	}
+
+	@Override
+	public boolean remove(E e) {
+		int removed =0;
+		for (int i =0; i < Data.length; i++){
+			if(Data[i].equals(e))
+				removed++;
+		}
+		if (removed > 0) {
+			Object[] newData = new Object[Data.length - removed];
+			for (int i = 0, j = 0; i< Data.length; i++) {
+				if (!Data[i].equals(e))
+					newData[j++]=Data[i];
+			}
+			Data=newData;
+		}
+		
+		return removed >0;
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
